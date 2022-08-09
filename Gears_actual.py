873a3f003a579c14914a65d89b23685bdf09e240
WeirdSam <109998039+WeirdSam@users.noreply.github.com>
2022-08-09 15:27:18 +1000

    initial

---
+++ Gears_actual.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 15:25:33 2022
+
+@author: User
+"""
+
+motor_power = 
+Kv = 
+wheel_rad = 
+winch_rad = 
+time = 1.5*60 #s
+
+w_motor = (Kv*V) * 2 * pi /60 #rad/s - 3000 is RPM
